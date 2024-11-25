3677fce7260c9a7fa633d49b03c9ee5d8510e698
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2024-11-26 00:05:24 +0530

    First commit

---
+++ src/main/java/co/sumit/SpringBootDemoBatchApplication.java
@@ -0,0 +1,13 @@
+package co.sumit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoBatchApplication.class, args);
+	}
+
+}
